fd84e3c3ba9358393a539c7026c7ad109678b221
Sajid <sajidsheikh1998@gmail.com>
2022-04-01 22:45:10 +0530

    added hash table

---
+++ basic-hash-table.js
@@ -0,0 +1,40 @@
+class HashTable {
+  constructor() {
+    this.size = 10000;
+    this.buckets = Array(this.size).fill(null);
+  }
+
+  hash(key) {
+    let hash = 0;
+    for (const char of key) {
+      hash += char.charCodeAt(0);
+    }
+    return hash % this.size;
+  }
+
+  set(key, value) {
+    const keyHash = this.hash(key);
+    this.buckets[keyHash] = value;
+  }
+
+  get(key) {
+    const keyHash = this.hash(key);
+    return this.buckets[keyHash];
+  }
+
+  showInfo() {
+    for (const key in this.buckets) {
+      if (this.buckets[key] !== null) {
+        console.log(key, this.buckets[key]);
+      }
+    }
+  }
+}
+
+const hash = new HashTable();
+
+hash.set("sajid", "sajid");
+hash.set("123", "123");
+hash.set("sheikh", "sheikh");
+// console.log(hash.get("123"));
+console.log(hash.showInfo(1));
