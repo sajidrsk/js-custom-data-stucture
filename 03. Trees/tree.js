02628e67e8d4b0b8bdfe741b358863df3351ce22
Sajid <sajidsheikh1998@gmail.com>
2022-04-03 15:23:39 +0530

    added filesystem-tree

---
+++ 03. Trees/tree.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value, parentNode = null) {
+    this.children = [];
+    this.parent = parentNode;
+    this.value = value;
+  }
+
+  addNode(value) {
+    const node = new Node(value, this);
+    this.children.push(node);
+    return { node: node, index: this.children.length - 1 };
+  }
+  removeNode(index) {
+    this.children.splice(index, 1);
+  }
+}
+
+class Tree {
+  constructor(rootValue) {
+    this.root = new Node(rootValue);
+  }
+}
+
+const filesystem = new Tree("/");
+const documentsNodeData = filesystem.root.addNode("/documents");
+const gamesNodeData = filesystem.root.addNode("/games");
+documentsNodeData.node.addNode("results.txt");
+gamesNodeData.node.addNode("call of duty.exe");
+
+console.log(filesystem);
